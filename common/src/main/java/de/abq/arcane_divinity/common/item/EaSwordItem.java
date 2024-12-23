b65b6a2ad5c3888b7d8577b5e8c142c21bd798dc
Cron3x <57831654+Cron3x@users.noreply.github.com>
2024-12-23 22:12:53 +0100

    ea sword spins fast after killing a mob + do not use loop in animation.json

--- common/src/main/java/de/abq/arcane_divinity/common/item/EaSwordItem.java
+++ common/src/main/java/de/abq/arcane_divinity/common/item/EaSwordItem.java
@@ -1,14 +1,18 @@
 package de.abq.arcane_divinity.common.item;
 
+import de.abq.arcane_divinity.ArcaneDivinity;
 import de.abq.arcane_divinity.common.defaulted.renderer.DefaultedItemRenderer;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 import software.bernie.geckolib.animatable.GeoItem;
@@ -24,7 +28,7 @@ public class EaSwordItem extends SwordItem implements GeoItem {
     public static final String IDENTIFIER = "ea_sword";
 
     private static final RawAnimation IDLE_ANIM = RawAnimation.begin().thenLoop("idle");
-    private static final RawAnimation USE_ANIM = RawAnimation.begin().thenLoop("idle"); //TODO: make use animation
+    private static final RawAnimation USE_ANIM = RawAnimation.begin().thenPlayXTimes("idle",10);
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 
     private boolean triggeredUse = false;
@@ -72,7 +76,25 @@ public class EaSwordItem extends SwordItem implements GeoItem {
 
     @Override
     public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, @NotNull Player player, @NotNull InteractionHand usedHand) {
-        if (level instanceof ServerLevel serverLevel) triggerAnim(player, GeoItem.getOrAssignId(player.getItemInHand(usedHand), serverLevel), "Use", "use");
+
+        if (level instanceof ServerLevel serverLevel)
+            triggerAnim(player, GeoItem.getOrAssignId(player.getItemInHand(usedHand), serverLevel), "Use", "use");
         return super.use(level, player, usedHand);
+    }
+
+    @Override
+    public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        ArcaneDivinity.LOG.debug("hurtEnemy");
+        if (attacker.level() instanceof ServerLevel serverLevel)
+            triggerAnim(attacker, GeoItem.getOrAssignId(attacker.getItemInHand(InteractionHand.MAIN_HAND), serverLevel), "Use", "use");
+        return super.hurtEnemy(stack, target, attacker);
+    }
+
+    @Override
+    public void postHurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        ArcaneDivinity.LOG.debug("postHurtEnemy");
+        if (attacker.level() instanceof ServerLevel serverLevel)
+            triggerAnim(attacker, GeoItem.getOrAssignId(attacker.getItemInHand(InteractionHand.MAIN_HAND), serverLevel), "Use", "use");
+        super.postHurtEnemy(stack, target, attacker);
     }
 }
