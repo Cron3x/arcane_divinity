8bea915c74f530a4fe6dacef6586368024da573b
Cron3x <57831654+Cron3x@users.noreply.github.com>
2024-12-26 19:18:29 +0100

    BlockEntityRenderers are now registered clientside only

--- neoforge/src/main/java/de/abq/arcane_divinity/ArcaneDivinityClientEntry.java
+++ neoforge/src/main/java/de/abq/arcane_divinity/ArcaneDivinityClientEntry.java
@@ -1,13 +1,17 @@
 package de.abq.arcane_divinity;
 
 
+import de.abq.arcane_divinity.client.renderer.Renderers;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.common.Mod;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 
-@Mod(value = ArcaneDivinity.MOD_ID, dist = Dist.CLIENT)
-public class ArcaneDivinityClientEntry {
-    public ArcaneDivinityClientEntry(IEventBus eventBus) {
-        ArcaneDivinity.initClient();
+@EventBusSubscriber(modid = ArcaneDivinity.MOD_ID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
+public final class ArcaneDivinityClientEntry {
+    @SubscribeEvent
+    public static void registerRenderers(final EntityRenderersEvent.RegisterRenderers event) {
+        Renderers.Entities.register(event::registerEntityRenderer);
+        Renderers.Blocks.register(event::registerBlockEntityRenderer);
     }
 }
